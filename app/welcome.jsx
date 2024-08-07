f0b964a1ebb8c1238834e31b76c20529ec28570d
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-07 22:58:39 +0530

    create login page and adding PNG cover photo -2024/08/07-

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import ScreenWrapper from "../components/ScreenWrapper";
+import {StatusBar} from "expo-status-bar";
+import {hp, wp} from "../helpers/common";
 
 const Welcome = () => {
-  return (
-    <ScreenWrapper style={styles.container}>
-      <Text>Welcome Component</Text>
-    </ScreenWrapper>
-  );
+    return (
+        <ScreenWrapper bg="white">
+            <StatusBar style="dark"/>
+                <View style={styles.container}>
+                        {/*login cover*/}
+                    <Image style={styles.welcomeImage} resizeMode='contain' source={require('../assets/images/login-cover.png')}/>
+                </View>
+        </ScreenWrapper>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        paddingHorizontal: wp(4),
+
+    },
+    welcomeImage: {
+        height: hp(30),
+        width: wp(100),
+        alignSelf: 'center',
+    },
+
 });
 
 export default Welcome;
