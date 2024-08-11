599960f8047d3fbf9a4caccd7086b67ad1edf184
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-11 20:00:02 +0530

    Done Login Button
    -2024/08/11-

--- app/login.jsx
+++ app/login.jsx
@@ -1,24 +1,110 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useRef, useState} from 'react';
+import {View, Text, StyleSheet, Pressable, Alert} from 'react-native';
 import ScreenWrapper from "../components/ScreenWrapper";
-import Home from "../assets/icons/Home";
+
 import {theme} from "../constants/theme";
+import Icon from "../assets/icons";
+import {useRouter} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import BackButton from "../components/BackButton";
+import {hp, wp} from "../helpers/common";
+import Input from "../components/Input";
+import Button from "../components/Button";
 
 const Login = () => {
-  return (
-    <ScreenWrapper >
-      <Text>Login</Text>
-      <Home strokeWidth={2} color={theme.colors.rose}/>
-    </ScreenWrapper>
-  );
+    const router = useRouter();
+    const emailRef = useRef("");
+    const passwordRef = useRef("");
+    const [loading, setLoading] = useState(false);
+
+    const onSubmit = async () => {
+        if(!emailRef.current || !passwordRef.current){
+            Alert.alert("Sign Up", "please fill all the fields!");
+        }
+    }
+
+
+
+    return (
+        <ScreenWrapper>
+            <StatusBar style="dark"/>
+            <View style={styles.container}>
+                <BackButton router={router}/>
+
+                {/*Welcome*/}
+                <View>
+                    <Text style={styles.welcomeText}>Hey,</Text>
+                    <Text style={styles.welcomeText}>Welcome Back</Text>
+                </View>
+                {/*form*/}
+                <View style={styles.form}>
+                    <Text style={{fontSize: hp(1.5), color: theme.colors.text}}>
+                        Please login to continue
+                    </Text>
+                    <Input
+                        icon={<Icon name="mail" size={26} strokeWidth={1.6}/>}
+                        placeholder={'Enter your email'}
+                        onChangeText={value => emailRef.current = value}
+                    />
+                    <Input
+                        icon={<Icon name="lock" size={26} strokeWidth={1.6}/>}
+                        placeholder={'Enter your password'}
+                        sec
+                        onChangeText={value => passwordRef.current = value}
+                    />
+                    <Text style={styles.forgotPassword}>
+                        Forgot Password?
+                    </Text>
+                {/*  button  */}
+                    <Button title={'Login'} loading={loading} onPress={onSubmit}/>
+                </View>
+                {/*  footer  */}
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>
+                        Don't have an account?
+                    </Text>
+                    <Pressable onPress={() => router.push('signUp')}>
+                        <Text style={[styles.footerText, {
+                            color: theme.colors.primaryDark,
+                            fontWeight: theme.fonts.semibold
+                        }]}>Sign up</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </ScreenWrapper>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        gap: 45,
+        paddingHorizontal: wp(5),
+    },
+    welcomeText: {
+        fontSize: hp(4),
+        fontWeight: theme.fonts.bold,
+        color: theme.colors.text,
+    },
+    form: {
+        gap: 25,
+    },
+    forgotPassword: {
+        textAlign: 'right',
+        fontWeight: theme.fonts.semibold,
+        color: theme.colors.text,
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
+    },
+    footerText: {
+        textAlign: 'center',
+        color: theme.colors.text,
+        fontSize: hp(1.6),
+    },
 });
 
 export default Login;
