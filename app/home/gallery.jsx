120801cb27a7cdd57c2b4367c5dd47a5a494af7c
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-08 02:34:32 +0530

    Add video and import
    -2024/08/11-

--- app/home/gallery.jsx
+++ app/home/gallery.jsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { Video } from 'expo-av';
+import {hp} from "../../helpers/common";
+
+
+const { width, height } = Dimensions.get('window');
 
 const Gallery = () => {
+  const videoRef = useRef(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Gallery Component</Text>
-    </View>
+      <View style={styles.container}>
+        {/* Video Component */}
+        <Video
+            ref={videoRef}
+            source={require('../../assets/3997198-uhd_4096_2160_25fps.mp4')} // Replace with your actual video file path
+            style={styles.backgroundVideo}
+            resizeMode="cover"
+            shouldPlay
+            isLooping
+        />
+
+        {/* Text Overlay */}
+        <View style={styles.overlay}>
+          <Text style={styles.overlayText}>Experience {"\n"} the Elegance of {"\n"}ZenStyle
+            Salon</Text>
+        </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  backgroundVideo: {
+    width: width,
+    height: hp(30),
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  overlay: {
+    position: 'absolute',
+    top: hp(15), // Adjust the position as needed
+    left: '10%', // Adjust the position as needed
+  },
+  overlayText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
 
