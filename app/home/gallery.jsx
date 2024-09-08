f2072dfd9c39d8dc34756f569a7a3c370f007461
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-08 14:09:23 +0530

    gallery page upload
    -2024/09/08-

--- app/home/gallery.jsx
+++ app/home/gallery.jsx
@@ -1,10 +1,21 @@
-import React, { useRef } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {View, Text, StyleSheet, Dimensions, FlatList, Image} from 'react-native';
 import { Video } from 'expo-av';
 import {hp} from "../../helpers/common";
+import Marquee from 'react-native-marquee';
 
 
 const { width, height } = Dimensions.get('window');
+
+const logos = [
+    require('../../assets/images/gallery/happy-womens-day-celebration-card-with-flowers-line-face.png'),
+    require('../../assets/images/gallery/gradient-hair-salon-logo-template.png'),
+    require('../../assets/images/gallery/toa-heftiba-ewpTmN9pQJM-unsplash.jpg'),
+    require('../../assets/images/gallery/toa-heftiba-ewpTmN9pQJM-unsplash.jpg'),
+    require('../../assets/images/gallery/toa-heftiba-ewpTmN9pQJM-unsplash.jpg'),
+    require('../../assets/images/gallery/toa-heftiba-ewpTmN9pQJM-unsplash.jpg'),
+    // Add more logos from your local assets directory
+];
 
 const Gallery = () => {
   const videoRef = useRef(null);
@@ -26,6 +37,19 @@ const Gallery = () => {
           <Text style={styles.overlayText}>Experience {"\n"} the Elegance of {"\n"}ZenStyle
             Salon</Text>
         </View>
+          <View style={styles.containerMarq}>
+              <Marquee
+
+                  style={styles.marquee}
+                  duration={100000} // Adjust duration based on speed
+                  marqueeOnStart
+              >
+                  {logos.map((logo, index) => (
+                      <Image key={index} source={logo} style={styles.logo} />
+                  ))}
+              </Marquee>
+          </View>
+
       </View>
   );
 };
@@ -47,10 +71,28 @@ const styles = StyleSheet.create({
     left: '10%', // Adjust the position as needed
   },
   overlayText: {
+
     color: 'white',
     fontSize: 24,
     fontWeight: 'bold',
   },
+    containerMarq: {
+        position: 'absolute',
+        bottom: hp(5), // Adjust position of marquee
+        width: width,
+        height: 100, // Adjust height based on your logo size
+        zIndex: 2, // Ensure FlatList is above the video
+    },
+    marquee: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 100, // Adjust width based on your logo size
+        height: 100, // Adjust height based on your logo size
+        borderRadius: 50,
+        marginHorizontal: 20,
+    },
 });
 
 export default Gallery;
