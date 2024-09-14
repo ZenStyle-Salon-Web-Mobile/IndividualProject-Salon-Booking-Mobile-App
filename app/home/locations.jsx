c93a0ab635b8c85e1044bb3559e2c4f975035b69
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-14 18:30:52 +0530

    pending location app
    -2024/09/14-

--- app/home/locations.jsx
+++ app/home/locations.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-const Locations = () => {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Locations Component</Text>
-    </View>
+      <View style={styles.container}>
+        <MapView
+            provider={PROVIDER_GOOGLE} // if you want to use Google Maps
+            style={styles.map}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            />
+      </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
-
-export default Locations;
