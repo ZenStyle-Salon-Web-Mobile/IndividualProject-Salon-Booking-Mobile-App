f67ed7187fc680f25f3935ebbb6dc0818e56ddb7
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-15 16:23:16 +0530

    location page done
    -2024/09/14-

--- app/home/locations.jsx
+++ app/home/locations.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import index from "../index";
+import {CustomMarker, markers} from "../../components/CustomMarker";
 
-export default function App() {
-  return (
-      <View style={styles.container}>
-        <MapView
-            provider={PROVIDER_GOOGLE} // if you want to use Google Maps
-            style={styles.map}
-            region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-            />
-      </View>
-  );
+export default function Locations() {
+    const region = {
+        latitude: 6.923987843102914,
+        longitude: 79.87092292394051,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+    };
+
+    return (
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                region={region}
+                showsUserLocation={true}
+                showsMyLocationButton={true}
+                loadingEnabled={true} // Shows a loading indicator until the map is loaded
+                onError={(error) => {
+                    console.log('Map error: ', error);
+                }}
+            >
+                {/* You can add markers to the map */}
+                {
+                    markers.map((marker, index) => (
+                        <Marker key={index}
+                                title={marker.title}
+                                description={marker.name}
+                                coordinate={marker}
+                        />
+                    ))
+                }
+
+                {/*<Marker*/}
+                {/*    coordinate={{*/}
+                {/*        latitude: 6.923987843102914,*/}
+                {/*        longitude: 79.87092292394051,*/}
+                {/*    }}*/}
+                {/*    title={"ZenStyle Salon"}*/}
+                {/*    description={"Beauty Category"}*/}
+                {/*/>*/}
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject, // Fill the entire View
+    },
 });
