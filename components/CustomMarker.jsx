ae84a17bba1b0e87f579421b12cf3898102422eb
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-15 16:22:58 +0530

    add location file
    -2024/09/14-

---
+++ components/CustomMarker.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import MapView, { Callout, Marker } from 'react-native-maps';
+
+export const markers = [
+    {
+        latitude: 6.923987843102914,
+        longitude: 79.87092292394051,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+        title: 'ZenStyle Salon',
+        name: 'Nugegoda Branch'
+    },
+    {
+        latitude: 6.866292584592846,
+        longitude: 79.8731523805743,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+        title: 'ZenStyle Salon',
+        name: 'Dehiwala Branch'
+
+    },
+    {
+        latitude: 6.894638773010035,
+        longitude: 79.91006263457079,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+        title: 'ZenStyle Salon',
+        name: 'Battaramulla Branch'
+    },
+
+]
+
