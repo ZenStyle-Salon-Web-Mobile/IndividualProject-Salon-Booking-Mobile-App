6439553e91ddbcd78d0d6aaecfb772347a20bce9
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-16 10:23:15 +0530

    image mapping file add
    -2024/09/16-

---
+++ components/imageMapping.js
@@ -0,0 +1,14 @@
+// imageMapping.js
+const imageMapping = {
+    'image1': require('../assets/images/gallery/andrea-donato-S_pFZUt6eKQ-unsplash.jpg'),
+    'image2': require('../assets/images/gallery/chalo-garcia-9pszsLaXh7A-unsplash.jpg'),
+    'image3': require('../assets/images/gallery/toa-heftiba-ewpTmN9pQJM-unsplash.jpg'),
+    'image8': require('../assets/images/gallery/gradient-hair-salon-logo-template.png'),
+    'image4': require('../assets/images/gallery/hand-drawn-happy-women-s-day-background.png'),
+    'image5': require('../assets/images/gallery/happy-womens-day-celebration-card-with-flowers-line-face.png'),
+    'image6': require('../assets/images/gallery/nail-salon-logo-template-design.png'),
+    'image7': require('../assets/images/gallery/spa-floral-badge.png'),
+    // Add more images as needed
+};
+
+export default imageMapping;
