df62ef379411d784e1f6d6e738de0354f203a722
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-16 15:02:02 +0530

    contact pending screen done
    -2024/09/16-

--- app/home/contact.jsx
+++ app/home/contact.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef} from 'react';
-import {StyleSheet, ScrollView, View, Dimensions, Pressable, Linking} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, ScrollView, View, Dimensions, Pressable, Linking, Text} from 'react-native';
 import Animated, {useAnimatedStyle, useSharedValue, withDelay, withTiming} from "react-native-reanimated";
 import {Image} from "expo-image";
 import {theme} from "../../constants/theme";
@@ -10,36 +10,20 @@ import tiktokLogo from '../../assets/images/social-logos/icons8-tiktok-480.png';
 import twitterLogo from '../../assets/images/social-logos/icons8-twitter-144.png';
 import youtubeLogo from '../../assets/images/social-logos/icons8-youtube-480.png';
 import whatsappLogo from '../../assets/images/social-logos/icons8-whatsapp-480.png';
+import {hp} from "../../helpers/common";
+import {Entypo, FontAwesome, Fontisto, MaterialIcons, SimpleLineIcons} from "@expo/vector-icons";
+import Svg, {Defs, LinearGradient, Rect, Stop} from "react-native-svg";
 
 
 const {width: SCREEN_WIDTH} = Dimensions.get('window');
 
 const logos = [
-    {
-        source: facebookLogo,
-        website: 'https://web.facebook.com',
-    },
-    {
-        source: instagramLogo,
-        website: 'https://www.instagram.com',
-    },
-    {
-        source:tiktokLogo,
-        website:'https://www.tiktok.com',
-    },
-    {
-        source:twitterLogo,
-        website:'https://x.com',
-    },
-    {
-        source: youtubeLogo,
-        website: 'https://www.youtube.com',
-    },
-    {
-        source:whatsappLogo,
-        website: 'https://web.whatsapp.com',
-    },
-    // Add more logos from your local assets directory
+    {source: facebookLogo, website: 'https://web.facebook.com'},
+    {source: instagramLogo, website: 'https://www.instagram.com'},
+    {source: tiktokLogo, website: 'https://www.tiktok.com'},
+    {source: twitterLogo, website: 'https://x.com'},
+    {source: youtubeLogo, website: 'https://www.youtube.com'},
+    {source: whatsappLogo, website: 'https://web.whatsapp.com'},
 ];
 
 const Contact = () => {
@@ -58,44 +42,104 @@ const Contact = () => {
 
     return (
         <View style={styles.container}>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={styles.scrollView}
 
-            >
-                {logos.map((logo, index) => {
-                    const animatedStyle = useAnimatedStyle(() => {
-                        return {
+            {/* Contact Info Section */}
+            <View style={styles.contactSection}>
+                <Text style={styles.contactTitle}>Contact Info</Text>
+                <Text style={styles.textContent}>
+                    We would love to respond to your queries & help you out. Feel free to get in touch with us. Premier
+                    destination for professional hair care and beauty services, delivering award-winning excellence and
+                    personalized treatments since 2009.
+                </Text>
+            </View>
+
+            {/* Logo Scroll Section */}
+            <View style={styles.logoScrollSection}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.scrollView}
+                >
+                    {logos.map((logo, index) => {
+                        const animatedStyle = useAnimatedStyle(() => ({
                             transform: [
-                                {
-                                    translateX: withDelay(index * 100, withTiming(0, {duration: 500})),
-                                },
-                                {
-                                    scale: withDelay(index * 100, withTiming(1, {duration: 500})),
-                                },
+                                {translateX: withDelay(index * 100, withTiming(0, {duration: 500}))},
+                                {scale: withDelay(index * 100, withTiming(1, {duration: 500}))},
                             ],
                             opacity: withDelay(index * 100, withTiming(1, {duration: 500})),
-                        };
-                    });
+                        }));
 
-                    return (
-                        <Animated.View key={index} style={[styles.logoContainer, animatedStyle]}>
-                            <Pressable key={index}
-                                       onPress={() => handleLogoPress(logo.website)}>
-                                <Image source={logo.source} style={styles.logo}/>
-                            </Pressable>
-                        </Animated.View>
-                    );
-                })}
-            </ScrollView>
+                        return (
+                            <Animated.View key={index} style={[styles.logoContainer, animatedStyle]}>
+                                <Pressable onPress={() => handleLogoPress(logo.website)}>
+                                    <Image source={logo.source} style={styles.logo}/>
+                                </Pressable>
+                            </Animated.View>
+                        );
+                    })}
+                </ScrollView>
+            </View>
+
+            {/* Additional Section (Address Section) */}
+            <View style={{flex: 1, marginVertical: 10, marginHorizontal:30,}}>
+                <View style={styles.addressSection}>
+                    <SimpleLineIcons name="location-pin" size={24} color="black"/>
+                    <Text style={{fontWeight: theme.fonts.bold, fontSize: hp(2.8), paddingTop: 5}}>
+                        Address
+                    </Text>
+                    <Text style={styles.addressText}>No.6, Pagoda Road,
+                        Nugegoda, 10250, Sri Lanka.</Text>
+                </View>
+                <View style={styles.addressSection}>
+                    <FontAwesome name="mobile-phone" size={24} color="black"/>
+                    <Text style={{fontWeight: theme.fonts.bold, fontSize: hp(2.8), paddingTop: 5}}>
+                        Phone Number
+                    </Text>
+                    <Text style={styles.addressText}>123 Main St, Salon City</Text>
+                </View>
+                <View style={styles.addressSection}>
+                    <Fontisto name="email" size={24} color="black"/>
+                    <Text style={{fontWeight: theme.fonts.bold, fontSize: hp(2.8), paddingTop: 5}}>
+                        E-mail
+                    </Text>
+                    <Text style={styles.addressText}>123 Main St, Salon City</Text>
+                </View>
+                <View style={styles.addressSection}>
+                    <MaterialIcons name="access-time" size={24} color="black"/>
+                    <Text style={{fontWeight: theme.fonts.bold, fontSize: hp(2.8), paddingTop: 5}}>
+                        Opening Time
+                    </Text>
+                    <Text style={styles.addressText}>9.00 AM - 07.00 PM Tuesday - Sunday</Text>
+                </View>
+            </View>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        padding: 10,
+        flex: 1,
+        padding: 20,
+        justifyContent: 'space-between', // Adjust spacing between sections
+    },
+    contactSection: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    contactTitle: {
+        fontSize: hp(4),
+        fontWeight: theme.fonts.extraBold,
+        paddingVertical: 10,
+    },
+    textContent: {
+        textAlign: 'center',
+        fontSize: hp(1.6),
+        fontWeight: theme.fonts.medium,
+        marginBottom: 20,
+    },
+    logoScrollSection: {
+        justifyContent: 'center',
     },
     scrollView: {
         flexDirection: 'row',
@@ -112,6 +156,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffacd9',
         borderRadius: theme.radius.xxl,
         resizeMode: 'contain',
+    },
+    addressSection: {
+        marginVertical: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    addressText: {
+        fontSize: hp(2),
+        marginTop: 10,
+        textAlign: 'center',
     },
 });
 
