9af15f44f9b0442f59477f104b06e2fce6ff3268
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-24 18:04:36 +0530

    bug fix
    -2024/09/2-

--- app/home/ratings.jsx
+++ app/home/ratings.jsx
@@ -14,7 +14,7 @@ const Ratings = () => {
       <Text>Ratings Component</Text>
       <Text>Ratings Component</Text>
       <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>d
+      <Text>Ratings Component</Text>dk
     </View>
   );
 };
