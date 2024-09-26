f15139bf53cfcf9aa1468cacd91ed3791e5a3635
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-26 14:43:12 +0530

    create ratings page pending
    -2024/09/26-

--- app/home/ratings.jsx
+++ app/home/ratings.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import {View, Text, StyleSheet, Pressable} from 'react-native';
 import {theme} from "../../constants/theme";
 import {hp, wp} from "../../helpers/common";
+import {FontAwesome} from "@expo/vector-icons";
 
 const Ratings = () => {
+
+    const rating= 4.5;
+    // Create an array for star rendering
+    const totalStars = 5; // Assuming 5 stars
+    const stars = [];
+
+    // Loop through total stars to render filled and empty stars
+    for (let i = 1; i <= totalStars; i++) {
+        stars.push(
+            <FontAwesome
+                key={i}
+                name={i <= rating ? 'star' : 'star-o'} // 'star' for filled, 'star-o' for empty
+                size={30} // Size of the star
+                color="#ffd700" // Gold color for stars
+            />
+        );
+    }
 
 
     return (
         <View style={styles.container}>
             <View style={styles.ratingContainer}>
-                <Text>Overall Rating</Text>
-                <Text>3.9</Text>
-
+                <Text style={{fontSize: hp(3.7)}}>Overall Rating</Text>
+                <Text style={{fontSize: hp(12), fontWeight: theme.fonts.bold}}>3.9</Text>
+                <View style={styles.starContainer}>
+                    {stars}
+                </View>
             </View>
             <Pressable style={styles.reviewButton}>
                 <Text style={styles.reviewButtonText}>
@@ -42,8 +62,11 @@ const styles = StyleSheet.create({
     ratingContainer: {
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    starContainer: {
+        flexDirection: 'row',
 
-    }
+    },
 });
 
 export default Ratings;
