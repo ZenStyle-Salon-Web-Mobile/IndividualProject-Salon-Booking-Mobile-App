610c204ed0048d8ce0bd062b2572db18ce74b8d4
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-26 13:11:18 +0530

    bug fix
    -2024/09/26-

--- app/home/ratings.jsx
+++ app/home/ratings.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Ratings = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>
-      <Text>Ratings Component</Text>dkggggTYUTU
-    </View>
-  );
+
+
+    return (
+        <View style={styles.container}>
+
+            <Text>Ratings Component</Text>
+
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default Ratings;
