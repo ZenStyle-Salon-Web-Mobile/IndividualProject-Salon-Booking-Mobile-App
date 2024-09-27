02594adddd5a2cdbfbbeca3307c830932372f896
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-09-28 01:00:11 +0530

    done bugs -2024/09/26-

--- app/home/homepage.jsx
+++ app/home/homepage.jsx
@@ -13,6 +13,7 @@ const HomePage = () => {
       <Text>Notifications</Text>
       <Text>Notifications</Text>
       <Text>Notifications</Text>
+      <Text>Notifications</Text>
     </View>
   );
 };
