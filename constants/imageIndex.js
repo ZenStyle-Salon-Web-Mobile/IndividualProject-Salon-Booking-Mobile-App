dbf809e963eb4d1ea556f2524ea1576300e7a0f8
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-11 16:55:39 +0530

    create home page image -2024/10/11-

---
+++ constants/imageIndex.js
@@ -0,0 +1,8 @@
+
+export const sliderImages = [
+
+    require('../assets/images/promotions/slider5.jpg'),
+    require('../assets/images/promotions/slider4.jpg'),
+    require('../assets/images/promotions/slider3.jpg'),
+    require('../assets/images/promotions/slider2.jpg'),
+]
