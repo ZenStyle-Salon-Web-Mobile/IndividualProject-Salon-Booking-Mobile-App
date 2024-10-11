5ee4f677eb96dff640091c1b17c053ed6dbaa5d8
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-11 20:07:25 +0530

    create home page bug -2024/10/11-

--- app/home/homepage.jsx
+++ app/home/homepage.jsx
@@ -70,6 +70,7 @@ const HomePage = () => {
                 <Text style={styles.subTopic}>Top Services</Text>
                 <Text style={styles.subTopic}>Top Services</Text>
                 <Text style={styles.subTopic}>Top Services</Text>
+                <Text style={styles.subTopic}>Top Services</Text>
             </View>
 
             <View style={styles.carouselOffer}>
