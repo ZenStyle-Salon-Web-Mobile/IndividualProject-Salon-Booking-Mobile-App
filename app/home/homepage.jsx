47b6ba75d9fae09499395ee1347f13d04dfab330
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-11 16:55:23 +0530

    create home page -2024/10/11-

--- app/home/homepage.jsx
+++ app/home/homepage.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import {Ionicons} from "@expo/vector-icons";
-import {Colors} from "react-native/Libraries/NewAppScreen";
-import {theme} from "../../constants/theme";
-import {hp} from "../../helpers/common";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../../helpers/common";
+import Carousel from 'react-native-reanimated-carousel';
+import { sliderImages } from "../../constants/imageIndex";
 
 const HomePage = () => {
 
@@ -25,15 +26,14 @@ const HomePage = () => {
         <View style={styles.container}>
             <View>
                 <TouchableOpacity
-                    onPress={() => {
-                    }}
-                    style={{marginVertical: 10}}
+                    onPress={() => {}}
+                    style={{ marginVertical: 10 }}
                 >
                     <Image
                         source={{
                             uri: "https://xsgames.co/randomusers/assets/avatars/male/77.jpg"
                         }}
-                        style={{width: 50, height: 50, borderRadius: 50}}
+                        style={{ width: 50, height: 50, borderRadius: 50 }}
                     />
                 </TouchableOpacity>
                 <Text style={styles.subHeadingText}>
@@ -43,22 +43,54 @@ const HomePage = () => {
                     {greeting}
                 </Text>
             </View>
-            <View style={styles.carousalOffer}>
-                <Text style={styles.subTopic}>
-                    Special Offers
-                </Text>
-            </View>
-            <View style={styles.carousalOffer}>
-                <Text style={styles.subTopic}>
-                    Top Services
-                </Text>
-            </View>
-            <View style={styles.carousalOffer}>
-                <Text style={styles.subTopic}>
-                    Hair Specialist
-                </Text>
+
+            <View style={styles.carouselOffer}>
+                <Text style={styles.subTopic}>Special Offers</Text>
+                <Carousel
+                    loop
+                    width={wp(100)}
+                    height={hp(25)}
+                    autoPlay={true}
+                    autoPlayInterval={4000}
+                    data={sliderImages}
+                    scrollAnimationDuration={1000}
+                    // Enabling parallax-horizontal mode
+                    mode="parallax-horizontal"
+                    parallaxScrollingScale={0.9}
+                    parallaxScrollingOffset={50}
+                    renderItem={({ item, index, animatedValue }) => (
+                        <ItemCard item={item} index={index} animatedValue={animatedValue} />
+                    )}
+                />
             </View>
 
+            <View style={styles.carouselOffer}>
+                <Text style={styles.subTopic}>Top Services</Text>
+            </View>
+
+            <View style={styles.carouselOffer}>
+                <Text style={styles.subTopic}>Hair Specialist</Text>
+            </View>
+        </View>
+    );
+};
+
+const ItemCard = ({ item }) => {
+    return (
+        <View style={{  width: wp(100) - 70,   // Full width with 70 units of horizontal space
+            height: hp(25),        // 25% of the screen height
+            marginHorizontal: 10,  // Horizontal space between images
+            borderRadius: 20,      // Optional: Adds some rounding to image edges
+            overflow: 'hidden', }}>
+            <Image
+                source={item}
+                style={{
+                    width: '100%',          // Full width of the container
+                    height: '100%',         // Full height of the container (25% of screen height)
+                    resizeMode: 'cover',    // Ensures image covers the container
+                }}
+
+            />
         </View>
     );
 };
@@ -68,23 +100,22 @@ const styles = StyleSheet.create({
         flex: 1,
         marginHorizontal: 20
     },
-    HeadingText:{
-        fontWeight:theme.fonts.extraBold,
+    HeadingText: {
+        fontWeight: theme.fonts.extraBold,
         fontSize: hp(3),
     },
-    subHeadingText:{
+    subHeadingText: {
         color: theme.colors.text,
         fontSize: hp(1.5),
     },
-    carousalOffer:{
-        marginVertical:10,
+    carouselOffer: {
+        marginVertical: 10,
     },
-    subTopic:{
-        fontWeight:theme.fonts.extraBold,
+    subTopic: {
+        fontWeight: theme.fonts.extraBold,
         fontSize: hp(2.5),
         color: theme.colors.primaryDark,
     },
-
 });
 
 export default HomePage;
