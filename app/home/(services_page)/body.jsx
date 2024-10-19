1575e7722bc84c2bbb255a8832b12608936cec2b
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-19 20:38:16 +0530

    home page header update
    -2024/10/19-

--- app/home/(services_page)/body.jsx
+++ app/home/(services_page)/body.jsx
@@ -9,6 +9,7 @@ const Body = () => {
         <Text>Body Component</Text>
         <Text>Body Component</Text>
         <Text>Body Component</Text>
+        <Text>Body Component</Text>
     </View>
   );
 };
