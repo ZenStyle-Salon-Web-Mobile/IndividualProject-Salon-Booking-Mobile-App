688cb8e38c6ec4870f680466749a6706ab347e16
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-20 18:55:26 +0530

    done create promotion page
    -2024/10/20-

--- app/home/promotions.jsx
+++ app/home/promotions.jsx
@@ -142,6 +142,14 @@ const Promotions = () => {
             showsHorizontalScrollIndicator={false}
             pagingEnabled
         />
+        <FlatList
+            data={firstPromo}
+            renderItem={renderBannerItem}
+            keyExtractor={item => item.id}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
+        />
       </View>
   );
 };
