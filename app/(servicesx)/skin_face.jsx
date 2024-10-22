f0fed4737fe46f87142e8f28fd4284b65a5bc9b4
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-22 20:00:30 +0530

    done services style -2024/10/22-

--- app/(servicesx)/skin_face.jsx
+++ app/(servicesx)/skin_face.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, FlatList , Image} from 'react-native';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
