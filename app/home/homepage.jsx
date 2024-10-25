2022ecc0f83cb0316d303134e31a5fcdcc6e6399
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-26 02:35:36 +0530

    set time
    -2024/10/26-

--- app/home/homepage.jsx
+++ app/home/homepage.jsx
@@ -51,6 +51,50 @@ const data = [
 
 const HomePage = () => {
 
+    const saleEndDate = new Date();
+    saleEndDate.setDate(saleEndDate.getDate() + 2);
+    saleEndDate.setHours(23, 59, 59);
+
+    const [timeUnits, setTimeUnits] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    });
+
+    useEffect(() => {
+        const calculateTimeUnits = (timeDifference: number) => {
+            const seconds = Math.floor(timeDifference / 1000);
+            setTimeUnits({
+                days: Math.floor((seconds % (365 * 24 * 60 * 60)) / (24 * 60 * 60)),
+                hours: Math.floor((seconds % (24 * 60 * 60)) / (60 * 60)),
+                minutes: Math.floor((seconds % (60 * 60)) / 60),
+                seconds: seconds % 60,
+            })
+        }
+
+        const updateCountdown = () => {
+            const currentDate = new Date().getTime();
+            const expiryTime = saleEndDate.getTime();
+            const timeDifference = expiryTime - currentDate;
+
+            if (timeDifference <= 0) {
+                calculateTimeUnits(0);
+            } else {
+                calculateTimeUnits(timeDifference);
+            }
+        };
+
+        updateCountdown();
+        const interval = setInterval(updateCountdown, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const formatTime = (time: number) => {
+        return time.toString().padStart(2, "0");
+    };
+
     const Card = ({item, navigation}) => {
         return (
             <TouchableOpacity
@@ -167,7 +211,15 @@ const HomePage = () => {
                 showsVerticalScrollIndicator={false}
                 showsHorizontalScrollIndicator={false}
             >
-                <Text style={styles.subTopic}>Special Offers</Text>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 10}}>
+                    <Text style={styles.subTopic}>Special Offers</Text>
+                    <View style={styles.timer}>
+                        <Ionicons name='time-outline' size={16} color={'black'}/>
+                        <Text
+                            style={styles.timerText}>{`${formatTime(timeUnits.days)}:${formatTime(timeUnits.hours)}:${formatTime(timeUnits.minutes)}:${formatTime(timeUnits.seconds)}`}</Text>
+                    </View>
+                </View>
+
                 {/* Carousel Section */}
                 <View style={styles.carouselContainer}>
                     <Carousel
@@ -351,6 +403,18 @@ const styles = StyleSheet.create({
         fontWeight: themes.fonts.bold,
         fontSize: hp(1.5),
     },
+    timer: {
+        flexDirection: 'row',
+        gap: 5,
+        backgroundColor: '#F4CE14',
+        paddingHorizontal: 8,
+        paddingVertical: 5,
+        borderRadius: 12,
+    },
+    timerText: {
+        color: '#333',
+        fontWeight: '500'
+    }
 });
 
 export default HomePage;
