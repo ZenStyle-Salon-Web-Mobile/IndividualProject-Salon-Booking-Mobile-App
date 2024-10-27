41360f8b6900acff42fb2cef53675e67c89a4ec1
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-27 10:25:08 +0530

    create id file passed prop
    -2024/10/27-

--- app/listing/[id].jsx
+++ app/listing/[id].jsx
@@ -1,57 +1,90 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
 import {Stack, useLocalSearchParams} from "expo-router";
 import {hp, wp} from "../../helpers/common";
 import {themes} from "../../constants/themes";
 import {FontAwesome} from "@expo/vector-icons";
+import Animated, {
+    interpolate,
+    useAnimatedRef,
+    useAnimatedScrollHandler,
+    useAnimatedStyle,
+    useScrollViewOffset, useSharedValue
+} from "react-native-reanimated";
 
+const IMG_HEIGHT = hp(40);
 const ListingDetails = () => {
     const {id, image, rating, duration, price, description,} = useLocalSearchParams();
+
+    const scrollOffset = useSharedValue(0);
+
+    const scrollHandler = useAnimatedScrollHandler({
+        onScroll: (event) => {
+            scrollOffset.value = event.contentOffset.y;
+        },
+    });
+
+    const imageAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateY: interpolate(
+                        scrollOffset.value,
+                        [-IMG_HEIGHT, 0, IMG_HEIGHT],
+                        [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
+                    ),
+                },
+            ],
+        };
+    });
 
     return (
         <>
             <Stack.Screen options={{
                 headerTransparent: true,
                 title: `${id || ''}`
-
             }}/>
             <View style={styles.container}>
-                <Image source={image} style={styles.image}/>
-                <View style={styles.contentWrapper}>
-                    <View style={{flexDirection: 'column'}}>
-                        <Text style={styles.textID}>{id}</Text>
-                        <View style={{flexDirection: 'row', alignContent: 'center', alignItems: 'center'}}>
-                            <FontAwesome
-                                name={"star"} // Filled or outlined stars
-                                size={22}
-                                color={"#FFD700"} // Gold for filled, gray for outline
-                                style={{
-                                    shadowColor: '#000',
-                                    shadowOpacity: 9.2,
-                                    shadowRadius: 5,
-                                    shadowOffset: {width: 0, height: 2},
-                                    elevation: 10,
-                                }}
-                            />
-                            <Text style={styles.rating}>Rating: {rating}</Text>
+                <Animated.ScrollView  onScroll={scrollHandler}
+                                      scrollEventThrottle={16}
+                                      contentContainerStyle={{paddingBottom: 150}}>
+                    <Animated.Image source={image} style={[styles.image, imageAnimatedStyle]}/>
+                    <View style={styles.contentWrapper}>
+                        <View >
+                            <Text style={styles.textID}>{id}</Text>
+                            <View style={{flexDirection: 'row', alignContent: 'center', alignItems: 'center'}}>
+                                <FontAwesome
+                                    name={"star"} // Filled or outlined stars
+                                    size={20}
+                                    color={"#FFD700"} // Gold for filled, gray for outline
+                                    style={{
+                                        shadowColor: '#000',
+                                        shadowOpacity: 9.2,
+                                        shadowRadius: 5,
+                                        shadowOffset: {width: 0, height: 2},
+                                        elevation: 10,
+                                    }}
+                                />
+                                <Text style={styles.rating}>Rating: {rating}</Text>
+                            </View>
+                        </View>
+                        <View style={{flexDirection: 'column', justifyContent: 'flex-end'}}>
+                            <Text style={styles.textDuration}>{duration}</Text>
                         </View>
                     </View>
-                    <View style={{flexDirection: 'column', justifyContent: 'flex-end'}}>
-                        <Text style={styles.textDuration}>{duration}</Text>
-                    </View>
-                </View>
-                <View style={{alignContent: 'center', alignItems: 'center'}}>
+                    <View style={{alignContent: 'center', alignItems: 'center'}}>
 
-                    <Text style={styles.para}>{description}</Text>
-                </View>
-                <View style={styles.footer}>
-                    <TouchableOpacity style={styles.bookingBtn}>
-                        <Text style={styles.textBook}>Book Now</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.priceBtn}>
-                        <Text style={styles.textPrice}>{price}</Text>
-                    </TouchableOpacity>
-                </View>
+                        <Text style={styles.para}>{description}</Text>
+                    </View>
+                </Animated.ScrollView>
+            </View>
+            <View style={styles.footer}>
+                <TouchableOpacity style={styles.bookingBtn}>
+                    <Text style={styles.textBook}>Book Now</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.priceBtn}>
+                    <Text style={styles.textPrice}>{price}</Text>
+                </TouchableOpacity>
             </View>
         </>
 
@@ -61,25 +94,27 @@ const ListingDetails = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor:themes.colors.textLight,
     },
     image: {
         width: '100%',
-        height: hp(40),
+        height: IMG_HEIGHT,
     },
     rating: {
         paddingHorizontal: 10,
         color: '#666',
         fontWeight: themes.fonts.bold,
-        fontSize: hp(2.5),
+        fontSize: hp(2),
     },
     contentWrapper: {
         flexDirection: 'row',
-        padding: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
         justifyContent: "space-between",
         backgroundColor: themes.colors.subColor
     },
     textID: {
-        fontSize: hp(5),
+        fontSize: hp(4.5),
         fontWeight: themes.fonts.extraBold,
         letterSpacing: 0.5,
         paddingBottom: 5,
@@ -91,11 +126,9 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
     },
     textPrice: {
-        fontSize: hp(3),
+        fontSize: hp(1.8),
         fontWeight: themes.fonts.bold,
-        letterSpacing: 0.5,
-        paddingBottom: 5,
-
+        color: 'white',
     },
     para: {
         fontSize: hp(1.9),
@@ -105,27 +138,36 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         textAlign: "auto"
     },
-    footer:{
-        position:'absolute',
-        bottom:0,
-        padding:20,
-        paddingBottom:30,
-        width:wp(100),
+    footer: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        padding: 20,
+        paddingBottom: 30,
+        width: wp(100),
     },
-    bookingBtn:{},
-    priceBtn:{
-        backgroundColor:themes.colors.dark,
-        padding:20,
-        borderRadius:10,
-        alignItems:'center',
-
+    bookingBtn: {
+        flex: 2,
+        backgroundColor: themes.colors.primary,
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        marginRight: 20,
     },
-    textBook:{
+    priceBtn: {
+        flex: 1,
+        backgroundColor: themes.colors.dark,
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textBook: {
         fontSize: hp(3),
         fontWeight: themes.fonts.bold,
         letterSpacing: 0.5,
-        paddingBottom: 5,
-
+        textTransform: 'uppercase',
+        color: themes.colors.darkLight,
     },
 });
 
