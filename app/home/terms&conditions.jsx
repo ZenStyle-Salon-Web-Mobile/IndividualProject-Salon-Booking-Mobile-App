72b5149c3cb964cb1f50fc5203459223e6b4590f
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-10-28 02:01:48 +0530

    add actual data -2024/10/27-

--- app/home/terms&conditions.jsx
+++ app/home/terms&conditions.jsx
@@ -7,7 +7,7 @@ import {themes} from "../../constants/themes";
 const blogData = [{
     id: '1',
     title: '1) Registration and Account Verification',
-    paragraph: 'Users are required to register on the Salon Liyo website to book appointments. The registration process involves providing personal information such as name, contact number, email address, residential address, postal code, and city. Verification of the user\'s mobile number is necessary to activate the account.',
+    paragraph: 'Users are required to register on the Salon ZenStyle website to book appointments. The registration process involves providing personal information such as name, contact number, email address, residential address, postal code, and city. Verification of the user\'s mobile number is necessary to activate the account.',
     date: '11th Jan 2022',
 }, {
     id: '2',
@@ -22,22 +22,22 @@ const blogData = [{
 }, {
     id: '4',
     title: '4) Service Limitations and Liability',
-    paragraph: 'Specific services such as hair coloring, straightening, keratin treatments, and facials include a mandatory, free allergy test that must be conducted at least two days before the scheduled service. Salon Liyo will not be held responsible for any adverse effects or medical conditions that arise due to treatments administered at the salon or resulting from failure to conduct the allergy tests.',
+    paragraph: 'Specific services such as hair coloring, straightening, keratin treatments, and facials include a mandatory, free allergy test that must be conducted at least two days before the scheduled service. Salon ZenStyle will not be held responsible for any adverse effects or medical conditions that arise due to treatments administered at the salon or resulting from failure to conduct the allergy tests.',
     date: '24th Sept 2021',
 }, {
     id: '5',
     title: '5) Data Privacy',
-    paragraph: 'Salon Liyo respects the privacy of its clients. Personal data collected during registration and booking is used solely for processing appointments and will not be shared with third parties, except as required by law.',
+    paragraph: 'Salon ZenStyle respects the privacy of its clients. Personal data collected during registration and booking is used solely for processing appointments and will not be shared with third parties, except as required by law.',
     date: '24th Sept 2021',
 }, {
     id: '6',
     title: '6) Dispute Resolution',
-    paragraph: 'In the event of a dispute, customers are encouraged to contact our customer service team through the provided hotline or email. Salon Liyo commits to resolving disputes amicably and efficiently.',
+    paragraph: 'In the event of a dispute, customers are encouraged to contact our customer service team through the provided hotline or email. Salon ZenStyle commits to resolving disputes amicably and efficiently.',
     date: '24th Sept 2021',
 }, {
     id: '7',
     title: '7) Modifications to Terms',
-    paragraph: 'Salon Liyo reserves the right to amend these terms and conditions at any time. All changes will be effective immediately upon their posting on the website. Continued use of the booking system after any changes constitutes acceptance of the new terms.',
+    paragraph: 'Salon ZenStyle reserves the right to amend these terms and conditions at any time. All changes will be effective immediately upon their posting on the website. Continued use of the booking system after any changes constitutes acceptance of the new terms.',
     date: '24th Sept 2021',
 }, {
     id: '8',
@@ -47,7 +47,7 @@ const blogData = [{
 }, {
     id: '9',
     title: '9) Legal Compliance',
-    paragraph: 'Salon Liyo operates in compliance with applicable local and national laws. Users agree to not use the booking system for any unlawful activities.',
+    paragraph: 'Salon ZenStyle operates in compliance with applicable local and national laws. Users agree to not use the booking system for any unlawful activities.',
     date: '24th Sept 2021',
 },];
 
