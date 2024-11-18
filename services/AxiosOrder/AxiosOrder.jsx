fee244dd7a9ab43227a0a3bfee4ef9502e9e2343
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-18 20:22:06 +0530

    update back end port
    -2024-11-12-

--- services/AxiosOrder/AxiosOrder.jsx
+++ services/AxiosOrder/AxiosOrder.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8082',
+    baseURL: 'http://localhost:8080',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
