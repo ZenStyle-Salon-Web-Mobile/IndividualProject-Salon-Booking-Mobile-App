dfb743920a6b149336fc37918ee1b24270ba5f97
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-18 20:27:42 +0530

    update
    -2024-11-18-

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -104,7 +104,7 @@ const SignUp = () => {
             {/*  button  */}
             <Button title={'Sign up'} loading={loading} onPress={onSubmit}/>
           </View>
-          {/*  footer 12345678910 */}
+          {/*  footer 1234567891011 */}
           <View style={styles.footer}>
             <Text style={styles.footerText}>
               Already have an account
