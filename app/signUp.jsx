55c43cab318056cab3826304b6b230cb49d26cc4
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-20 23:15:07 +0530

    update
    -2024-11-20-

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -106,7 +106,7 @@ const SignUp = () => {
           </View>
           {/*  footer */}
           {/*  footer 12345678910*/}
-          {/*  foot*/}
+          {/*  foo */}
           <View style={styles.footer}>
             <Text style={styles.footerText}>
               Already have an account
