df75221a6e83d60dafe47265c82c147c99d54b07
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-12-09 09:58:42 +0530

    axios order update -2024-12-09-

--- services/AxiosOrder/AxiosOrder.jsx
+++ services/AxiosOrder/AxiosOrder.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
-    timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+    baseURL: 'http://ec2-13-201-82-187.ap-south-1.compute.amazonaws.com:8080',
+    timeout: 5000,
+    // headers: {'X-Custom-Header': 'foobar'}
 });
 
 export default instance;
